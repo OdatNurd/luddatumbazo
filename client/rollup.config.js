09a289fe295984fecced7f6ae3c9a7fbd2958567
Terence Martin <odatnurd@gmail.com>
2023-12-11 22:48:26 -0800

    Out with old (ui), in with the new (ui)
    After a brief ride on the struggle bus, this replaces the placeholder UI
    that used Windstorm in favor of a new version that uses Zephyr instead.
    
    The content attempts to be a fairly close match to what we had before,
    though there may be some styling issues, and for the moment the button
    doesn't work properly.

---
+++ client/rollup.config.js
@@ -0,0 +1,39 @@
+import 'dotenv/config';
+
+import svelte from 'rollup-plugin-svelte';
+import resolve from '@rollup/plugin-node-resolve';
+import replace from '@rollup/plugin-replace';
+import commonjs from '@rollup/plugin-commonjs';
+import copyStatic from '@axel669/rollup-copy-static';
+import $path from '@axel669/rollup-dollar-path';
+
+export default [
+  {
+    input: 'src/index.js',
+    output: {
+      file: `../dist/app.js`,
+      format: 'iife',
+      name: 'app',
+    },
+    plugins: [
+      svelte({}),
+      $path({
+          root: ".",
+          // paths: {
+          //   $components: "src/client/components/index.js",
+          //   $stores: "src/client/stores/index.js",
+          //   $lib: "src/client/lib",
+          // },
+          extensions: [".js", ".mjs", ".svelte", ".jsx"]
+      }),
+      replace(
+      {
+        'preventAssignment': true,
+        'process.env.GAME_API_ROOT_URI': JSON.stringify(process.env.GAME_API_ROOT_URI),
+      }),
+      commonjs(),
+      resolve({ browser: true }),
+      copyStatic("static")
+    ],
+  }
+]
---
+++ client/src/App.svelte
@@ -0,0 +1,34 @@
+<script>
+  import { wsx } from "@axel669/zephyr";
+  import { Screen, Paper, Grid, Titlebar, Text, Link, Icon } from "@axel669/zephyr";
+
+  console.log(`Game API URI: ${process.env.GAME_API_ROOT_URI}`);
+</script>
+
+<svelte:body use:wsx={{"$theme": 'dark', "$app": true, "p": "8px"}} />
+
+<Screen>
+  <Paper>
+    <Titlebar slot="header">
+      <Text slot="title" title>
+        Luddatumbazo!
+        <Text subtitle>Exactly like BoardGameGeek except not</Text>
+      </Text>
+
+      <Link button flat slot="action" href="/cdn-cgi/access/logout">
+        <Icon name="logout"></Icon>
+      </Link>
+    </Titlebar>
+
+    <Grid>
+      <p>If this project were more well defined, there would be content here. </p>
+      <p>Alas.</p>
+    </Grid>
+
+    <Titlebar slot="footer">
+      <Text slot="title" title>
+        <Text subtitle>Let the turds hit the floor</Text>
+      </Text>
+    </Titlebar>
+  </Paper>
+</Screen>
