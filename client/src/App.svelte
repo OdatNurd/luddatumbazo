d796f7c3e51e44d179f80baae84d233f5453f2d2
Terence Martin <odatnurd@gmail.com>
2023-12-12 22:34:51 -0800

    Implement some core basic pages
    These are currently absolutely awful in the Repeat Yourself area, but
    for the purpose of quickly sketching out how we want the page to look
    and feel, it's good enough and we can clean it up later.

--- client/src/App.svelte
+++ client/src/App.svelte
@@ -2,23 +2,25 @@
   import { wsx } from "@axel669/zephyr";
   import { Screen, Paper, Grid, Flex, Titlebar, Text, Link, Icon } from "@axel669/zephyr";
 
-  import { LoadZone, DataTable, Th, sorts, filters } from "@axel669/zephyr";
+  import Router from 'svelte-spa-router';
+  import {location} from 'svelte-spa-router'
 
-  const API = `${process.env.GAME_API_ROOT_URI}/api/v1`;
-  console.log(`Game API URI: ${API}`);
+  import GameList from './pages/games/List.svelte';
+  import CategoryList from './pages/metadata/CategoryList.svelte';
+  import MechanicList from './pages/metadata/MechanicList.svelte';
+  import DesignerList from './pages/metadata/DesignerList.svelte';
+  import ArtistList from './pages/metadata/ArtistList.svelte';
+  import Publisherst from './pages/metadata/PublisherList.svelte';
 
-  // Waiting for Godot
-  const godot = (time) => new Promise(resolve => setTimeout(resolve, time * 1000));
-
-  // Fetch the list of games from the back end API, and return the result.
-  const loadGames = async () => {
-    const gameListURI = `${API}/game/list`;
-    await godot(0.5);
-
-    const res = await fetch(gameListURI);
-    return await res.json();
-  };
-
+  const routes = {
+    // Exact path
+    '/games': GameList,
+    '/categories': CategoryList,
+    '/mechanics': MechanicList,
+    '/designers': DesignerList,
+    '/artists': ArtistList,
+    '/publishers': Publisherst,
+  }
 </script>
 
 <svelte:body use:wsx={{ "$theme": 'dark', "$app": true, "p": "8px" }} />
@@ -31,46 +33,25 @@
         <Text subtitle>Exactly like BoardGameGeek except not</Text>
       </Flex>
 
+      <Link button outline slot="menu" t.dec="none" href="#/">
+        <Icon name="home"></Icon>
+      </Link>
+
       <Link button outline slot="action" t.dec="none" href="/cdn-cgi/access/logout">
         <Icon name="logout"></Icon>
       </Link>
     </Titlebar>
 
+    <Flex direction="row">
+      <Link href="#/games">Games</Link>
+      <Link href="#/categories">Categories</Link>
+      <Link href="#/mechanics">Mechanics</Link>
+      <Link href="#/designers">Designers</Link>
+      <Link href="#/artists">Artists</Link>
+      <Link href="#/publishers">Publishers</Link>
+    </Flex>
 
-    <LoadZone source={loadGames()} let:result>
-      <DataTable data={result.data} pageSize={20} color="primary">
-        <svelte:fragment slot="header">
-          <Th w="64px" sort={sorts.natural("id")}>ID</Th>
-          <Th filter={filters.text("name")} sort={sorts.natural("name")}>Name</Th>
-          <Th w="64px">Ext</Th>
-        </svelte:fragment>
-        <svelte:fragment slot="row" let:row>
-          <td>{row.id}</td>
-          <td><a href="/games/{row.slug}">{row.name}</a></td>
-          <td>
-            {#if row.bggId !== 0}
-              <Link target="_blank" href="https://boardgamegeek.com/boardgame/{row.bggId}/">
-                BGG <Icon name="external-link"></Icon>
-              </Link>
-            {:else}
-              --
-            {/if}
-          </td>
-        </svelte:fragment>
-      </DataTable>
-    </LoadZone>
-
-<!--
-    <Grid>
-       <ul>
-        <li>The overall list of games</li>
-        <li>The overall list of metadata X/5</li>
-        <li>Details on a specific game</li>
-        <li>All games matching metadata X/5</li>
-      </ul>
-    </Grid>
- -->
-
+    <Router {routes}/>
 
     <Titlebar slot="footer">
       <Text slot="title" title>
---
+++ client/src/pages/metadata/ArtistList.svelte
@@ -0,0 +1,43 @@
+<script>
+  import { Link, Icon } from "@axel669/zephyr";
+
+  import { LoadZone, DataTable, Th, sorts, filters } from "@axel669/zephyr";
+
+  const API = `${process.env.GAME_API_ROOT_URI}/api/v1`;
+  console.log(`Game API URI: ${API}`);
+
+  // Waiting for Godot
+  const godot = (time) => new Promise(resolve => setTimeout(resolve, time * 1000));
+
+  // Fetch the list of games from the back end API, and return the result.
+  const loadGames = async () => {
+    const gameListURI = `${API}/game/meta/artist/list`;
+    // await godot(0.25);
+
+    const res = await fetch(gameListURI);
+    return await res.json();
+  };
+</script>
+
+<LoadZone source={loadGames()} let:result>
+  <DataTable data={result.data} pageSize={20} color="primary">
+    <svelte:fragment slot="header">
+      <Th w="64px" sort={sorts.natural("id")}>ID</Th>
+      <Th filter={filters.text("name")} sort={sorts.natural("name")}>Name</Th>
+      <Th w="64px">Ext</Th>
+    </svelte:fragment>
+    <svelte:fragment slot="row" let:row>
+      <td>{row.id}</td>
+      <td><a href="/artists/{row.slug}">{row.name}</a></td>
+      <td>
+        {#if row.bggId !== 0}
+          <Link target="_blank" href="https://boardgamegeek.com/boardgameartist/{row.bggId}/">
+            BGG <Icon name="external-link"></Icon>
+          </Link>
+        {:else}
+          --
+        {/if}
+      </td>
+    </svelte:fragment>
+  </DataTable>
+</LoadZone>
