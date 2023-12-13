b9f8c10ae5471ab03c07e296aa67d7dad459da11
Terence Martin <odatnurd@gmail.com>
2023-12-12 20:21:01 -0800

    Implement some data table action for the game list
    As a simple test of tying everything together, this implements a very
    simple page which displays the list of games that are currently known
    to the system.
    
    This is very simplistic at the moment as we try to remind ourselves how
    Svelte works in general, and as we try to decide what exactly all of the
    desired pages are actually going to look like.

--- client/src/App.svelte
+++ client/src/App.svelte
@@ -1,8 +1,24 @@
 <script>
   import { wsx } from "@axel669/zephyr";
-  import { Screen, Paper, Grid, Titlebar, Text, Link, Icon } from "@axel669/zephyr";
+  import { Screen, Paper, Grid, Flex, Titlebar, Text, Link, Icon } from "@axel669/zephyr";
 
-  console.log(`Game API URI: ${process.env.GAME_API_ROOT_URI}`);
+  import { LoadZone, DataTable, Th, sorts, filters } from "@axel669/zephyr";
+
+  const API = `${process.env.GAME_API_ROOT_URI}/api/v1`;
+  console.log(`Game API URI: ${API}`);
+
+  // Waiting for Godot
+  const godot = (time) => new Promise(resolve => setTimeout(resolve, time * 1000));
+
+  // Fetch the list of games from the back end API, and return the result.
+  const loadGames = async () => {
+    const gameListURI = `${API}/game/list`;
+    await godot(0.5);
+
+    const res = await fetch(gameListURI);
+    return await res.json();
+  };
+
 </script>
 
 <svelte:body use:wsx={{ "$theme": 'dark', "$app": true, "p": "8px" }} />
@@ -20,10 +36,41 @@
       </Link>
     </Titlebar>
 
+
+    <LoadZone source={loadGames()} let:result>
+      <DataTable data={result.data} pageSize={20} color="primary">
+        <svelte:fragment slot="header">
+          <Th w="64px" sort={sorts.natural("id")}>ID</Th>
+          <Th filter={filters.text("name")} sort={sorts.natural("name")}>Name</Th>
+          <Th w="64px">Ext</Th>
+        </svelte:fragment>
+        <svelte:fragment slot="row" let:row>
+          <td>{row.id}</td>
+          <td><a href="/games/{row.slug}">{row.name}</a></td>
+          <td>
+            {#if row.bggId !== 0}
+              <Link target="_blank" href="https://boardgamegeek.com/boardgame/{row.bggId}/">
+                BGG <Icon name="external-link"></Icon>
+              </Link>
+            {:else}
+              --
+            {/if}
+          </td>
+        </svelte:fragment>
+      </DataTable>
+    </LoadZone>
+
+<!--
     <Grid>
-      <p>If this project were more well defined, there would be content here. </p>
-      <p>Alas.</p>
+       <ul>
+        <li>The overall list of games</li>
+        <li>The overall list of metadata X/5</li>
+        <li>Details on a specific game</li>
+        <li>All games matching metadata X/5</li>
+      </ul>
     </Grid>
+ -->
+
 
     <Titlebar slot="footer">
       <Text slot="title" title>
