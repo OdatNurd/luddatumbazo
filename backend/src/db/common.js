9cb3660a99844d7fb8ff7d01dd91a840ae716538
Terence Martin <odatnurd@gmail.com>
2023-12-16 22:43:39 -0800

    Include a facility to map internal image paths
    This commit implements a system by which one of our internal images can
    be converted into a URL based on configured options and the purpose for
    which the image is going to be put.
    
    This uses an internal URI scheme of cfi:// to represent Cloudflare
    images, since we plan to eventually have multiple back ends.
    
    The routine can convert such a scheme into a proper URL based on the
    configured environment variables.

--- backend/src/db/common.js
+++ backend/src/db/common.js
@@ -1,6 +1,21 @@
 /******************************************************************************/
 
 
+/* This maps our own internal name for image variants to the variants that
+ * should be actually used as a part of the expansion.
+ *
+ * The keys in this list are the internal string names for the image types that
+ * we want to use, and the values are the names of the environment variables
+ * that tell us what names to use. */
+const variantMap = {
+  'boxart': 'CF_VARIANT_BOXART',
+  'thumbnail': 'CF_VARIANT_THUMB'
+}
+
+
+/******************************************************************************/
+
+
 /* Given some information on where and what database action is taken, what the
  * result was, and whether or not it is a batch, display a log that displays
  * details of the operation. */
@@ -62,6 +77,75 @@ export const getDBResult = (where, action, resultSet) => {
   // Just a single result set, so log it and return the inner results back.
   displayDBResultLog(where, action, resultSet, false);
   return resultSet.results;
+}
+
+
+/******************************************************************************/
+
+
+
+/* This takes as a argument an asset path from a database record that represents
+ * an image and an image variant name, and returns back the full URL to the
+ * image for that variant that allows it to be displayed.
+ *
+ * Since many image assets are optional, this will gracefully return the input
+ * value if it is not set. It will however throw an exception if the image
+ * variant requested is not valid. */
+export function getImageAssetURL(ctx, assetPath, imageVariant) {
+  // Images are optional; don't try to do anything fancy if there is no image.
+  if (assetPath === '') {
+    return assetPath;
+  }
+
+  // Look up the image variant we were given to see what environment variable
+  // we should look in; fail if this does not map.
+  const variantEnvName = variantMap[imageVariant];
+  if (variantEnvName === undefined) {
+    throw new Error(`unknown image variant '${imageVariant}`);
+  }
+
+  // Look up the actual variant name by using the environment; fail if the
+  // variable is not set.
+  const variant = ctx.env[variantEnvName];
+  if (variant === undefined) {
+    throw new Error(`environment variable '${variantEnvName}' is not set`);
+  }
+
+  // Parse the incoming asset path to get at the root URL;  verify that the
+  // protocol is what we expect; we will ultimately support multiple image
+  // back ends.
+  const url = new URL(assetPath);
+  if (url.protocol !== 'cfi:') {
+    throw new Error(`unknown image storage format ${url.protocol}`);
+  }
+
+  // The imagePath is the path portion of the URL, except that it has a leading
+  // slash on it, which we want to get rid of.
+  const imagePath = url.pathname.substr(1);
+
+  // Get the image URL template for Cloudflare images out of the environment
+  // and then replace our template variables into it.
+  let imageUrl = ctx.env.CF_IMAGEURL_TEMPLATE;
+  imageUrl = imageUrl.replace('{imagePath}', imagePath);
+  imageUrl = imageUrl.replace('{variant}', variant);
+
+  return imageUrl;
+}
+
+
+/******************************************************************************/
+
+
+/* This takes as an argument an array of objects which have at their inputKey
+ * a value that should be transformed into an asset url via getAssetURL into
+ * a field named outputKey and returns back a list of items so modified.
+ *
+ * The input key will be removed and replaced with the mapped output key. */
+export function mapImageAssets(ctx, data, inputKey, imageVariant) {
+  return data.map(item => {
+    item[inputKey] = getImageAssetURL(ctx, item[inputKey], imageVariant);
+    return item;
+  });
 }
 
 
