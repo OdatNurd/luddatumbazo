298753b15d1952adb079493702800e9c70a2cf4c
Terence Martin <odatnurd@gmail.com>
2024-01-27 21:02:01 -0800

    Alter the API function for fetching API data
    This small change allows the client side API code to determine both its
    own and the remote origin that it's been told about, so that it can
    properly determine whether or not it should set the option that tells
    the browser to ship credentials.
    
    The default (for same origin) is to do so, but for different origins,
    such as when we're working locally, this needs to be overridden.
    
    Old man Odat doesn't want to subvert security mechanisms and cause more
    CORS headaches than absolutely necessary.

--- client/src/lib/fetch.js
+++ client/src/lib/fetch.js
@@ -7,6 +7,28 @@
  * "hard coded" to the correct value. */
 const API = `${process.env.GAME_API_ROOT_URI}/api/v1`;
 
+/* Set the base options which can be extended by any options that the caller
+ * provides when invoking the API.
+ *
+ * Specifically, if the API is being served from a different origin than we are,
+ * the browser will not pass through the authorization JWT when we call fetch;
+ * in such a case we need to override the default credentials. */
+const baseOptions = (() => {
+  // Get the location of the local and remote ends of our connection.
+  const local = new URL(window.location);
+  const remote = new URL(API);
+
+  // When the origins are different, we need to explicitly turn on inclusion of
+  // credentials in the call. This also requires that the API end allow our
+  // origin specifically in the list of CORS origins.
+  const options = {
+    credentials: (local.origin === remote.origin) ? 'same-origin' : 'include'
+  }
+
+  console.log(`base options are: ${JSON.stringify(options)}`);
+  return options;
+})();
+
 
 /******************************************************************************/
 
@@ -17,13 +39,12 @@ const API = `${process.env.GAME_API_ROOT_URI}/api/v1`;
  * The return value is the return of the fetch itself, which is done against the
  * configured API endpoint. */
 export async function api(endpoint, options) {
-    // If we're doing local stuff, we need cors magic cause we don't operate
-    // on the same hostname.
-    options = { ...options };
-    options.credentials = 'include';
+  // Set up our options; these start with the base options, and then we overlay
+  // anything else that's required onto them.
+  options = { ...baseOptions, ...options };
 
-    // Perform the actual fetch.
-    return fetch(`${API}${endpoint}`, options);
+  // Perform the actual fetch.
+  return fetch(`${API}${endpoint}`, options);
 }
 
 
