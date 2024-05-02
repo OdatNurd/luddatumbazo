f7a4c7af90f441831b86b94b421f25502f752a7e
Terence Martin <odatnurd@gmail.com>
2024-05-01 22:48:39 -0700

    Augment our builds to be production ready
    This updates our build infrastructure for the client side code such that
    when a build happens it can determine if it is a production or
    development build and act accordingly.
    
    For the time being, the difference between the two is related to using
    the terser() plugin in a production build, so that the release versions
    have smaller output that downloads faster.

--- client/rollup.config.js
+++ client/rollup.config.js
@@ -1,28 +1,82 @@
+/******************************************************************************/
+
+
 import 'dotenv/config';
 
-import svelte from 'rollup-plugin-svelte';
-import resolve from '@rollup/plugin-node-resolve';
-import replace from '@rollup/plugin-replace';
 import commonjs from '@rollup/plugin-commonjs';
+import del from 'rollup-plugin-delete';
+import replace from '@rollup/plugin-replace';
+import resolve from '@rollup/plugin-node-resolve';
+import svelte from 'rollup-plugin-svelte';
+import terser from '@rollup/plugin-terser';
+
+import $path from '@axel669/rollup-dollar-path';
+import asuid from "@axel669/asuid/node";
 import copyStatic from '@axel669/rollup-copy-static';
 import html from '@axel669/rollup-html-input';
-import $path from '@axel669/rollup-dollar-path';
 
-import asuid from "@axel669/asuid/node";
 
-export default [
+/******************************************************************************/
+
+
+/* Determine if this build is production or not; by default we assume that all
+ * builds are not production unless they are explictly set that way. */
+const production = (process.env.NODE_ENV === 'production');
+console.log(`build mode: ${production ? 'production' : 'development'}`);
+
+
+/******************************************************************************/
+
+
+const buildList = [
   {
+    // Input for the build comes from the data-main script in this HTML file;
+    // both the resulting JS and the modified HTML file will be send to the
+    // output.
     input: 'src/index.html',
     output: {
       file: `public/app-${asuid()}.js`,
-      format: 'iife',
-      name: 'app',
+      format: 'esm',
+      sourcemap: true
     },
+
+    // Luxon has several circular dependency warnings that the author will not
+    // fix, so eat any warnings of this type specifically from that package.
+    onwarn(warning, handler) {
+      if (warning.code === 'CIRCULAR_DEPENDENCY') {
+        if(warning.message.includes('node_modules/luxon/')) {
+          return;
+        }
+      }
+      handler(warning);
+    },
+
     plugins: [
+      // Copy over the contents of the static folder; this happens only once
+      // when running a "watch" build.
+      copyStatic("static"),
+
+      // Remove the bundled JS output to make the generated output during
+      // development easier to grok.
+      del({
+        targets: [
+          "public/app-*.js",
+          "public/app-*.js.map"
+        ],
+        force: true
+      }),
+
+      // Do the processing that associates the input HTML with the output JS.
       html(),
+
+      // Compile svelte components; CSS is generated out to JS code that applies
+      // styles dynamically as needed.
       svelte({
         emitCss: false
       }),
+
+      // Translate import statements in the client source to make file locations
+      // less brittle.
       $path({
           root: ".",
           paths: {
@@ -32,16 +86,38 @@ export default [
             $lib: "src/lib",
             $api: "src/api/index.js"
           },
-          extensions: [".js", ".mjs", ".svelte", ".jsx"]
+          extensions: [".js", ".mjs", ".svelte"]
       }),
+
+      // Perform dynamic replacements of specific strings at build time; We use
+      // this to make the code appear to be reading the environment, even though
+      // it actually is not.
       replace(
       {
         'preventAssignment': true,
         'process.env.GAME_API_ROOT_URI': JSON.stringify(process.env.GAME_API_ROOT_URI),
       }),
+
+      // Ensure that modules resolve and that the bundler knows that the output
+      // is for a browser (Svelte will fail to compile files otherwise).
       commonjs(),
       resolve({ browser: true }),
-      copyStatic("static")
     ],
   }
-]
+];
+
+
+/******************************************************************************/
+
+
+/* When we do production builds, all of the targets in the build list should
+ * be augmented to include terser() to help minify the output, as the final
+ * build step. */
+if (production === true) {
+  buildList.forEach(build => build.plugins.push(terser()));
+}
+
+
+/******************************************************************************/
+
+export default buildList;
